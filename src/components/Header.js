b5d144bad2fd38ac55cff2ef0a469715c23bc6e5
JonGrabbe <jgrabbe69@gmail.com>
2023-03-18 06:59:37 -0700

    header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,14 @@
 import menuIcon from '../assets/shared/tablet/icon-hamburger.svg';
+import logo from '../assets/shared/desktop/logo.svg';
+import cart from '../assets/shared/desktop/icon-cart.svg';
 
 export default function Header(props) {
     return (
-    <header className="top-header">
+    <header className="top-header wrapper">
         <div className="menu">
-            <img src={menuIcon} alt="" />
+            <img src={menuIcon} alt="" className="menu-icon" />
+            <img src={logo} alt="" className="logo" />
+            <img src={cart} alt="" className="cart-icon" />
         </div>
         <div className="content"></div>
     </header>
