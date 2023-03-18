b5f0e0dbbdc1710bce7c5c19cfcb720a64761b7c
JonGrabbe <jgrabbe69@gmail.com>
2023-03-17 17:04:15 -0700

    added assets folder

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import './App.scss';
 function App() {
   return (
     <div className="App">
-      <h1>audio site</h1>
-      <p>hello this is my site</p>
+      
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import menuIcon from '../assets/'
+
+export default function Header(props) {
+    return (
+    <header className="top-header">
+        <div className="menu">
+            <img src="../../" alt="" />
+        </div>
+        <div className="content"></div>
+    </header>
+    )
+}
