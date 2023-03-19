c29832b097c33863483047b6df5138b21b12699c
JonGrabbe <jgrabbe69@gmail.com>
2023-03-18 17:26:57 -0700

    fixed spacing problem and centered see product link

--- src/components/header/Content.js
+++ src/components/header/Content.js
@@ -1,12 +1,14 @@
 export default function Content(props) {
     return (
         <div className="content">
-            <h2 className="item">new product</h2>
-            <h3 className="item">XX99 Mark II Headphones</h3>
-            <p className="item">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
-            <a href="" className="see-product-link item">
-                see product
-            </a>
+            <div className="items-container">
+                <h2 className="item">new product</h2>
+                <h3 className="item">XX99 Mark II Headphones</h3>
+                <p className="item">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+                <a href="" className="see-product-link item">
+                    see product
+                </a>
+            </div>
         </div>
     )
 }
