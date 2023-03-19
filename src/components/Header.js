5d6d5970224de5eb86c983f1216bf49a54a8ba7f
JonGrabbe <jgrabbe69@gmail.com>
2023-03-18 21:59:15 -0700

    menu button

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import React, {useState} from 'react'
 import menuIcon from '../assets/shared/tablet/icon-hamburger.svg';
 import logo from '../assets/shared/desktop/logo.svg';
 import cart from '../assets/shared/desktop/icon-cart.svg';
@@ -5,12 +6,15 @@ import Content from './header/Content';
 import MenuItems from './header/MenuItems';
 
 export default function Header(props) {
+    const [isOpen, setIsOpen] = useState(false);
     return (
     <header className="top-header wrapper">
         <div className="menu">
-            <img src={menuIcon} alt="" className="menu-icon" />
+            <button className="menu-button" onClick={() => setIsOpen(!isOpen)}>
+                <img src={menuIcon} alt="" className="menu-icon" />
+            </button>
             <img src={logo} alt="" className="logo" />
-            <MenuItems />
+            <MenuItems isOpen={isOpen} />
             <img src={cart} alt="" className="cart-icon" />
         </div>
         <Content />
--- src/components/header/MenuItems.js
+++ src/components/header/MenuItems.js
@@ -1,6 +1,12 @@
 export default function MenuItems(props) {
+    let cn = "menu-items";
+    if(props.isOpen) {
+        cn += " open"
+    } else {
+        cn = "menu-items"
+    }
     return (
-        <nav className="menu-items">
+        <nav className={cn}>
             <ul>
                 <li className="item"><a href="">Home</a></li>
                 <li className="item"><a href="">Headphones</a></li>
