d9e31c4c8857e96a43671aa9656b0adeb1e51c96
JonGrabbe <jgrabbe69@gmail.com>
2023-03-19 23:34:22 -0700

    styled mobile dropdown catagories

--- src/components/header/mobile-dropdown-menu/MobileDropdown.js
+++ src/components/header/mobile-dropdown-menu/MobileDropdown.js
@@ -12,8 +12,8 @@ export default function MobileDropdown(props) {
             <div className="wrapper">
                 <a href="" className="item">
                     <img src={headphonesImage} alt="" />
-                    <h3>Headphones</h3>
-                    <p>shop</p>
+                    <h3 className="title">Headphones</h3>
+                    <p className="shop-cta">shop</p>
                 </a>
                 <a href="" className="item">
                     <img src="" alt="" />
