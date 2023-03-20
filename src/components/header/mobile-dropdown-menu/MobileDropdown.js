33c17b4c887c21638b858fbfc9577b8e332eb2c9
JonGrabbe <jgrabbe69@gmail.com>
2023-03-19 23:43:58 -0700

    added svg arrow

--- src/components/header/mobile-dropdown-menu/MobileDropdown.js
+++ src/components/header/mobile-dropdown-menu/MobileDropdown.js
@@ -1,4 +1,5 @@
 import headphonesImage from '../../../assets/product-xx99-mark-one-headphones/mobile/image-product.png';
+import iconArrowRight from '../../../assets/shared/desktop/icon-arrow-right.svg';
 
 export default function MobileDropdown(props) {
     let cn = "mobile-dropdown-menu-container";
@@ -11,20 +12,25 @@ export default function MobileDropdown(props) {
         <div className={cn}>
             <div className="wrapper">
                 <a href="" className="item">
+                    <img className="thumbnail" src={headphonesImage} alt="" />
+                    <h3 className="title">Headphones</h3>
+                    <p className="shop-cta">
+                        shop 
+                        <img src={iconArrowRight} alt="" />
+                    </p>
+                    
+                </a>
+                <a href="" className="item">
                     <img src={headphonesImage} alt="" />
                     <h3 className="title">Headphones</h3>
                     <p className="shop-cta">shop</p>
                 </a>
                 <a href="" className="item">
-                    <img src="" alt="" />
-                    <h3>Headphones</h3>
-                    <p>shop</p>
+                    <img src={headphonesImage} alt="" />
+                    <h3 className="title">Headphones</h3>
+                    <p className="shop-cta">shop</p>
                 </a>
-                <a href="" className="item">
-                    <img src="" alt="" />
-                    <h3>Headphones</h3>
-                    <p>shop</p>
-                </a>
+                
             </div>
         </div>
     )
