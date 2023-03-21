506d0e4ea29a52f56a59d6b4bbf39cd96a1b6ca3
JonGrabbe <jgrabbe69@gmail.com>
2023-03-20 18:37:49 -0700

    categories component

--- src/components/categories/Categories.js
+++ src/components/categories/Categories.js
@@ -1,9 +1,43 @@
-import Item from "./Item"
+// import Item from "./Item"
+import headphonesImage from '../../../assets/product-xx99-mark-one-headphones/mobile/image-product.png';
+import iconArrowRight from '../../../assets/shared/desktop/icon-arrow-right.svg';
+import speakerImg from '../../../assets/home/mobile/image-speaker-zx9.png';
+import earphoneImg from '../../../assets/home/mobile/image-earphones-yx1-earphones.png';
 
 export default function Categories(props) {
     return (
-        <div className="categories-container">
-            
+        <div className="mobile-dropdown-menu-container">
+            <div className="wrapper">
+                <a href="" className="item headphones">
+                    <img className="thumbnail" src={headphonesImage} alt="" />
+                    <h3 className="title">Headphones</h3>
+                    <p className="shop-cta">
+                        shop 
+                        <img className="arrow" src={iconArrowRight} alt="" />
+                    </p>
+                    
+                </a>
+                <a href="" className="item speaker">
+                    <img className="thumbnail" src={speakerImg} alt="" />
+                    <h3 className="title">speakers</h3>
+                    <p className="shop-cta">
+                        shop 
+                        <img className="arrow" src={iconArrowRight} alt="" />
+                    </p>
+                    
+                </a>
+                <a href="" className="item earphones">
+                    <img className="thumbnail" src={earphoneImg} alt="" />
+                    <h3 className="title">Earphones</h3>
+                    <p className="shop-cta">
+                        shop 
+                        <img className="arrow" src={iconArrowRight} alt="" />
+                    </p>
+                    
+                </a>
+                
+                
+            </div>
         </div>
     )
 }
