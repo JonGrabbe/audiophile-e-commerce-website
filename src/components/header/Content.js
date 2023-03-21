ccc5e8b2b117f83691660cf8e91d437e8bf84128
JonGrabbe <jgrabbe69@gmail.com>
2023-03-20 20:09:08 -0700

    added html containers to content component

--- src/components/header/Content.js
+++ src/components/header/Content.js
@@ -1,13 +1,15 @@
 export default function Content(props) {
     return (
         <div className="content">
-            <div className="items-container">
-                <h2 className="item">new product</h2>
-                <h3 className="item">XX99 Mark II Headphones</h3>
-                <p className="item">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
-                <a href="" className="see-product-link item">
-                    see product
-                </a>
+            <div className="wrapper">
+                <div className="items-container">
+                    <h2 className="item">new product</h2>
+                    <h3 className="item">XX99 Mark II Headphones</h3>
+                    <p className="item">Experience natural, lifelike audio and exceptional build quality made for the passionate music enthusiast.</p>
+                    <a href="" className="see-product-link item">
+                        see product
+                    </a>
+                </div>
             </div>
         </div>
     )
