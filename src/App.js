34671156e89bad71adccc7ae4dfb7fa0139a0a21
JonGrabbe <jgrabbe69@gmail.com>
2023-03-21 13:45:24 -0700

    added css to Categories.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@
 import './scss/main.scss';
 
 import Header from './components/Header';
+import Categories from './components/categories/Categories';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* body */}
+      <main>
+        <Categories />
+      </main>
     </div>
   );
 }
