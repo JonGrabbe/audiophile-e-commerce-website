565518c8c16b674b3f4a8f78b7c24056a1ae78a4
JonGrabbe <jgrabbe69@gmail.com>
2023-03-22 16:54:23 -0700

    added html container

--- src/components/large-banner/LargeBanner.js
+++ src/components/large-banner/LargeBanner.js
@@ -6,14 +6,16 @@ export default function LargeBanner() {
             <div className="wrapper">
                 <div className="oval-background-outside oval"></div>
                 <div className="oval-background oval"></div>
-                <div className="left-container">
-                    <img className='speaker-img' src={speakerImg} alt="" />
-                </div>
-                <div className="right-container">
-                    <div className="wrapper">
-                        <h1 className="title">ZX9 <br /> SPEAKER</h1>
-                        <p>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
-                        <a href="" className="main-button link">See Product</a>
+                <div className="layout-container">
+                    <div className="left-container">
+                        <img className='speaker-img' src={speakerImg} alt="" />
+                    </div>
+                    <div className="right-container">
+                        <div className="wrapper">
+                            <h1 className="title">ZX9 <br /> SPEAKER</h1>
+                            <p>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
+                            <a href="" className="main-button link">See Product</a>
+                        </div>
                     </div>
                 </div>
             </div>
