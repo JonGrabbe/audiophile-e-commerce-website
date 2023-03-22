5d5c5984537204f3bffbda3d00885f914f03971e
JonGrabbe <jgrabbe69@gmail.com>
2023-03-22 00:16:39 -0700

    added decrative html ovals

--- src/components/large-banner/LargeBanner.js
+++ src/components/large-banner/LargeBanner.js
@@ -3,7 +3,8 @@ import speakerImg from '../../assets/home/mobile/image-speaker-zx9.png';
 export default function LargeBanner() {
     return (
         <div className="large-banner-container">
-            <div className="oval-background"></div>
+            <div className="oval-background-outside oval"></div>
+            <div className="oval-background oval"></div>
             <div className="left-container">
                 <img className='speaker-img' src={speakerImg} alt="" />
             </div>
