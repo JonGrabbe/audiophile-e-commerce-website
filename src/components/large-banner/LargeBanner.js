30f42f859f93aa150c6327be8e8d7b91816df6ed
JonGrabbe <jgrabbe69@gmail.com>
2023-03-22 02:32:34 -0700

    styled LargeBanner component bottom section

--- src/components/large-banner/LargeBanner.js
+++ src/components/large-banner/LargeBanner.js
@@ -8,6 +8,11 @@ export default function LargeBanner() {
             <div className="left-container">
                 <img className='speaker-img' src={speakerImg} alt="" />
             </div>
+            <div className="right-container">
+                <h1 className="title">ZX9 <br /> SPEAKER</h1>
+                <p>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
+                <a href="" className="main-button link">See Product</a>
+            </div>
         </div>
     )
 }
