968df31b4b0d946f780047fb1abba2616bd832ad
JonGrabbe <jgrabbe69@gmail.com>
2023-03-21 20:28:19 -0700

    worked on added css oval

--- src/components/large-banner/LargeBanner.js
+++ src/components/large-banner/LargeBanner.js
@@ -3,6 +3,7 @@ import speakerImg from '../../assets/home/mobile/image-speaker-zx9.png';
 export default function LargeBanner() {
     return (
         <div className="large-banner-container">
+            <div className="oval-background"></div>
             <div className="left-container">
                 <img className='speaker-img' src={speakerImg} alt="" />
             </div>
