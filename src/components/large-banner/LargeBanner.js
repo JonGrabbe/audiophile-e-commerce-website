0a070d492a508f30f0b5458796913681fad3d473
JonGrabbe <jgrabbe69@gmail.com>
2023-03-22 19:06:08 -0700

    centered .main-wrapper

--- src/components/large-banner/LargeBanner.js
+++ src/components/large-banner/LargeBanner.js
@@ -3,31 +3,17 @@ import speakerImg from '../../assets/home/mobile/image-speaker-zx9.png';
 export default function LargeBanner() {
     return (
         <div className="large-banner-container">
-            <div className="wrapper">
+            <div className="wrapper wrapper-container">
                 <div className="oval-background-outside oval"></div>
                 <div className="oval-background oval"></div>
-                <div className="layout-container">
-                    <div className="left-container">
-                        <img className='speaker-img' src={speakerImg} alt="" />
-                    </div>
-                    <div className="right-container">
-                        <div className="wrapper">
-                            <h1 className="title">ZX9 <br /> SPEAKER</h1>
-                            <p>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
-                            <a href="" className="main-button link">See Product</a>
-                        </div>
-                    </div>
+                <div className="left-container">
+                    <img className='speaker-img' src={speakerImg} alt="" />
                 </div>
-                <div className="layout-container-desktop">
-                    <div className="left-container-d">
-                        <img className='speaker-img-d' src={speakerImg} alt="" />
-                    </div>
-                    <div className="right-container-d">
-                        <div className="wrapper-d">
-                            <h1 className="title-d">ZX9 <br /> SPEAKER</h1>
-                            <p>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
-                            <a href="" className="main-button link-d">See Product</a>
-                        </div>
+                <div className="right-container">
+                    <div className="wrapper">
+                        <h1 className="title">ZX9 <br /> SPEAKER</h1>
+                        <p>Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.</p>
+                        <a href="" className="main-button link">See Product</a>
                     </div>
                 </div>
             </div>
