ed8e7a5aa6ebb7ebd9d1e7ee5338e6c708020991
JonGrabbe <jgrabbe69@gmail.com>
2023-03-24 21:27:07 -0700

    tablet breakpoint

--- src/components/text-image-banner/TextImageBanner.js
+++ src/components/text-image-banner/TextImageBanner.js
@@ -1,5 +1,3 @@
-import img from "../../assets/product-xx99-mark-two-headphones/mobile/image-gallery-1.jpg";
-
 export default function TextImageBanner() {
   return (
     <div className="text-image-banner-container">
