37bb2a00d7ce3187bdd796be43c0d760590c5541
JonGrabbe <jgrabbe69@gmail.com>
2023-03-24 21:15:23 -0700

    added text;

--- src/components/text-image-banner/TextImageBanner.js
+++ src/components/text-image-banner/TextImageBanner.js
@@ -1,9 +1,22 @@
-import img from '../../assets/product-xx99-mark-two-headphones//mobile/image-gallery-1.jpg';
+import img from "../../assets/product-xx99-mark-two-headphones/mobile/image-gallery-1.jpg";
 
 export default function TextImageBanner() {
-    return (
-        <div className="text-image-banner-container">
-            <div className="img-container"></div>
-        </div>
-    )
+  return (
+    <div className="text-image-banner-container">
+      <div className="img-container"></div>
+      <div className="text-container">
+        <h2 className="heading">
+          Bringing you the <span className="word">best</span> audio gear
+        </h2>
+        <p className="text-p">
+          Located at the heart of New York City, Audiophile is the premier store
+          for high end headphones, earphones, speakers, and audio accessories.
+          We have a large showroom and luxury demonstration rooms available for
+          you to browse and experience a wide range of our products. Stop by our
+          store to meet some of the fantastic people who make Audiophile the
+          best place to buy your portable audio equipment.
+        </p>
+      </div>
+    </div>
+  );
 }
