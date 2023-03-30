0ab5280da6e9c364c02c3878cd22aeca5fa0bf42
JonGrabbe <jgrabbe69@gmail.com>
2023-03-30 02:18:43 -0700

    worked on categories headphones route

--- src/routes/headphones/HeadphonesPage.js
+++ src/routes/headphones/HeadphonesPage.js
@@ -1,7 +1,12 @@
+import Header from "../../components/Header"
+import Footer from "../../components/footer/footer"
+
 export default function HeadphonesPage(props) {
     return (
         <div className="headphones">
-            <h1>headphones</h1>
+            <Header />
+            
+            <Footer />
         </div>
     )
 }
