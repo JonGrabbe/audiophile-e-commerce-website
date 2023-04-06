386366512b6f08b5a6c64bfc549ee131c88ec0da
JonGrabbe <jgrabbe69@gmail.com>
2023-04-06 14:37:23 -0700

    styled .new-product-item-container

--- src/components/categoriesPage/CategoriesPage.js
+++ src/components/categoriesPage/CategoriesPage.js
@@ -7,7 +7,7 @@ console.log(productsData.headphones)
 export default function CategoriesPage(props) {
     let arr = []
     return (
-        <main className="main-wrapper">
+        <main className="main-wrapper categories-page-container">
            {
             productsData.headphones.forEach(item => {
                 arr.push(<NewProductItem title={item.title} text={item.text} img={item.image} />)
--- src/routes/components/new-product-item/NewProductItem.js
+++ src/routes/components/new-product-item/NewProductItem.js
@@ -2,14 +2,14 @@ export default function NewProductItem(props) {
     return (
         <div className="new-product-item-container">
             <div className="left-container">
-                <img src={props.img} alt="" />
+                <img className="img" src={props.img} alt="" />
             </div>
             <div className="right-container">
                 <div className="content-container">
                     <h2 className="header">New product</h2>
                     <h3 className="title">{props.title}</h3>
-                    <p>{props.text}</p>
-                    <a href="" className="see-product-link">See product</a>
+                    <p className="text">{props.text}</p>
+                    <a href="" className="see-product-link link">See product</a>
                 </div>
             </div>
         </div>
