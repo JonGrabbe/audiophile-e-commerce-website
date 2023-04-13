bb7677f5c485a86a094dbfe3ab954f8c0605baae
JonGrabbe <jgrabbe69@gmail.com>
2023-04-13 10:20:19 -0700

    added styles

--- src/components/product-includes/ProductIncludes.js
+++ src/components/product-includes/ProductIncludes.js
@@ -6,9 +6,9 @@ function Item(props) {
     return (
         <div className="item-container">
             <div className="image-container">
-                <img src={props.img} alt="" />
+                <img className="img" src={props.img} alt="" />
             </div>
-            <h3>{props.title}</h3>
+            <h3 className="title">{props.title}</h3>
             <MainLink to={props.productLink} text="see product" />
         </div>
     )
@@ -17,6 +17,7 @@ function Item(props) {
 export default function ProductIncludes(props) {
     return (
         <div className="product-includes-container">
+            <h2 className="heading">You may also like</h2>
             {
                 props.others.map(item => (
                     <Item img={getImagePath(item.image.mobile, '../../assets/')} title={item.name} productLink={'/product/'+item.slug} />
