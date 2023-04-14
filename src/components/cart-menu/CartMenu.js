ef2eae243247db93b1eae62e191536eca6d6b748
JonGrabbe <jgrabbe69@gmail.com>
2023-04-14 14:46:45 -0700

    added styles

--- src/components/cart-menu/CartMenu.js
+++ src/components/cart-menu/CartMenu.js
@@ -37,22 +37,24 @@ export default function CartMenu(props) {
         }
     }
     return (
-        <div className="cart-blur-container">
-            <div className="cart-menu-container" style={style}>
-                <div className="top-container">
-                    <h2 className="heading">Cart </h2>
-                    <button className="remove-all-button">remove all</button>
-                </div>
-                {/* <div className="cart-items-container">
-                </div>
-                <div className="botton-container">
-                    <div className="total-container">
-                        <span className="title">Total</span>
-                        <span className="total">${props.cart.getTotal()}</span>
-                    </div>
-                    <a href="/checkout" className="checkout-link">checkout</a>
-                </div> */}
+        <div>
+            <div className="cart-blur-container">
             </div>
+                <div className="cart-menu-container" style={style}>
+                    <div className="top-container">
+                        <h2 className="heading">Cart </h2>
+                        <button className="remove-all-button">remove all</button>
+                    </div>
+                    {/* <div className="cart-items-container">
+                    </div>
+                    <div className="botton-container">
+                        <div className="total-container">
+                            <span className="title">Total</span>
+                            <span className="total">${props.cart.getTotal()}</span>
+                        </div>
+                        <a href="/checkout" className="checkout-link">checkout</a>
+                    </div> */}
+                </div>
         </div>
     )
 }
