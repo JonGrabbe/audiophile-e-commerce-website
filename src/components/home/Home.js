f016b17645080e1ac2b028705709989cee9baa6f
JonGrabbe <jgrabbe69@gmail.com>
2023-04-17 11:13:24 -0700

    addToCart function stores product object in array

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -13,94 +13,179 @@ import data from '../../data.json';
 
 export default function Home(props) {
   let cartObj = {
-
+    products: []
   }
   const [cart, setCart] = useState(cartObj)
   const [amountMap, setAmountMap] = useState({})
 
-  function getProductObj(slug) {
-    let obj;
-    data.forEach(item => {
-      if(item.slug === slug) {
-        obj = {
-          ...item
+  /* 
+    {
+      total: 0,
+      products: [
+        {
+          slug,
+          name,
+          amount: 1,
+          isInCart: true
         }
-      }
-    })
-    return obj
-  }
-
-  function getTotal(obj) {
-    let total = 0
-    for(let property in obj) {
-      total += obj[property].price
+      ]
     }
-    return total
-  }
-
-  // added product object to parent object
-  // if amount is undefiened it is assumed to
-  // be 1
-  function addToCart(slug) {
-    let newObj = {
-      ...cart
-    }
-    // if(newObj[slug].amount) {
-    //   newObj[slug] = {...getProductObj(slug), amount: newObj[slug].amount}
-    // } else {
-    //   newObj[slug] = getProductObj(slug)
-    //   newObj[slug].amount = 1
-    // }
-    newObj[slug] = getProductObj(slug)
-    newObj[slug].isInCart = true
-    if(!amountMap[slug]) {
-      amountMap[slug] = 1
-    } else {
-      newObj[slug].amount = amountMap[slug]
-    }
-    newObj[slug].total = getTotal(newObj)
-    setCart(newObj)
-  }
 
 
-  function changeAmount(slug, operation) {
-    let newObj = {
-      ...amountMap
-    }
-    console.log(slug, operation)
-    let p = newObj[slug];
-    function operate(num, operation) {
-      if(operation === '+') {
-        return num + 1
-      }
-      if(operation === '-' && num > 1) {
-        return num -= 1
-      }
-    }
-    if(newObj[slug]) {
-      if(operation === '+') {
-        newObj[slug] += 1
-      }
-      if(operation === '-' && p > 1) {
-        newObj[slug] -= 1
-      }
-    } else {
-      if(operation === '+') {
-        newObj[slug] = 2
-      }
-    }
-    setAmountMap(newObj)
-  }
+    function addProduct(slug, amount) {
 
-  function removeAll() {
-    setCart({})
-    setAmountMap({})
-  }
+    }
 
-  let cartProps = {
-    cart: cart,
-    amountMap: amountMap
-  }
+    function changeAmount(slug, operation) {
+      if(hasObject(slug)) {
+        cart[slug].amount += 1
+      } else {
+        addProduct(slug, 1)
+      }
+    }
+
+    function removeAll() {
+      setCart({})
+    }
+  */
+    
+    function getProductObj(slug) {
+      let obj;
+      data.forEach(item => {
+        if(item.slug === slug) {
+          obj = item
+        }
+      })
+      return obj
+    }
+
+    function getAmountMapVal(slug) {
+      if(amountMap[slug]) {
+        return amountMap[slug]
+      } else {
+        return 1
+      }
+    }
+
+    function addToCart(slug, amount) {
+      let newObj = {
+        ...cart
+      }
+      let isInArray = false;
+      console.log(newObj)
+      newObj.products.forEach((item, i) => {
+        if(item.slug === slug) {
+          isInArray = true
+          // newObj.products[i]
+        }
+      })
+      // if product already exists
+      if(isInArray) {
+        // let productObject = getProductObj(slug)
+        // productObject.amount = amount
+        // newObj.products.push(getProductObj(slug))
+        newObj.products.forEach((item, i) => {
+          if(item.slug === slug) {
+            newObj.products[i].amount = getAmountMapVal(slug)
+          }
+        })
+      }
+      // if the product isnt in the array
+      if(!isInArray) {
+        let productObj = getProductObj(slug)
+        productObj.amount = getAmountMapVal(slug)
+        newObj.products.push(productObj)
+        setCart(newObj)
+      }
+
+    }
+
+  // function getProductObj(slug) {
+  //   let obj;
+  //   data.forEach(item => {
+  //     if(item.slug === slug) {
+  //       obj = {
+  //         ...item
+  //       }
+  //     }
+  //   })
+  //   return obj
+  // }
+
+  // function getTotal(obj) {
+  //   let total = 0
+  //   for(let property in obj) {
+  //     total += obj[property].price
+  //   }
+  //   return total
+  // }
+
+  // // added product object to parent object
+  // // if amount is undefiened it is assumed to
+  // // be 1
+  // function addToCart(slug) {
+  //   let newObj = {
+  //     ...cart
+  //   }
+  //   // if(newObj[slug].amount) {
+  //   //   newObj[slug] = {...getProductObj(slug), amount: newObj[slug].amount}
+  //   // } else {
+  //   //   newObj[slug] = getProductObj(slug)
+  //   //   newObj[slug].amount = 1
+  //   // }
+  //   newObj[slug] = getProductObj(slug)
+  //   newObj[slug].isInCart = true
+  //   if(!amountMap[slug]) {
+  //     amountMap[slug] = 1
+  //   } else {
+  //     newObj[slug].amount = amountMap[slug]
+  //   }
+  //   newObj[slug].total = getTotal(newObj)
+  //   setCart(newObj)
+  // }
+
+
+  // function changeAmount(slug, operation) {
+  //   let newObj = {
+  //     ...amountMap
+  //   }
+  //   console.log(slug, operation)
+  //   let p = newObj[slug];
+  //   function operate(num, operation) {
+  //     if(operation === '+') {
+  //       return num + 1
+  //     }
+  //     if(operation === '-' && num > 1) {
+  //       return num -= 1
+  //     }
+  //   }
+  //   if(newObj[slug]) {
+  //     if(operation === '+') {
+  //       newObj[slug] += 1
+  //     }
+  //     if(operation === '-' && p > 1) {
+  //       newObj[slug] -= 1
+  //     }
+  //   } else {
+  //     if(operation === '+') {
+  //       newObj[slug] = 2
+  //     }
+  //   }
+  //   setAmountMap(newObj)
+  // }
+
+  // function removeAll() {
+  //   setCart({})
+  //   setAmountMap({})
+  // }
+
+  // let cartProps = {
+  //   cart: cart,
+  //   amountMap: amountMap
+  // }
+
+
+  
 
 
   return (
@@ -114,8 +199,8 @@ export default function Home(props) {
             <Route path="speakers" element={<CategoriesPage productsData={productsData} ProductType="speakers" />} />
             <Route path="earphones" element={<CategoriesPage productsData={productsData} ProductType="earphones" />} />
         </Route>
-        <Route path="/product" element={<Root amountMap={amountMap} handleRemoveAll={removeAll} cart={cart} handleChangeAmount={changeAmount} />}>
-          <Route path="/product/:id" element={<Product handleAddToCart={addToCart} handleChangeAmount={changeAmount} amountMap={amountMap} />} />
+        <Route path="/product" element={<Root />}>
+          <Route path="/product/:id" element={<Product handleAddToCart={addToCart}  />} />
         </Route>
       </Routes>
     </>
