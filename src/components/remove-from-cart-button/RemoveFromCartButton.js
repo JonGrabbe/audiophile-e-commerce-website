d153f55a15eb288bf02003b6e41f27be0bc5688e
JonGrabbe <jgrabbe69@gmail.com>
2023-04-24 22:43:16 -0700

    added to cart button styled

--- src/components/remove-from-cart-button/RemoveFromCartButton.js
+++ src/components/remove-from-cart-button/RemoveFromCartButton.js
@@ -1,7 +1,7 @@
 export default function RemoveFromCart(props) {
     return (
         <div className="remove-from-cart-button-container">
-            <button onClick={() => props.handleRemoveItem(props.slug)}>
+            <button className="remove-from-cart-button main-button" onClick={() => props.handleRemoveItem(props.slug)}>
                 remove from cart
             </button>
         </div>
